549e8f545aeac9cdb9580a79700aa977182b0b06
Abdi <Dhagaxg@gmail.com>
2025-06-17 13:46:04 +0200

    Room Painting
    This program reads two types of input:
    The number of available paint can sizes and the number of paint needs.
    
    It then reads in the actual available cans and the needed cans. For each paint need, the program finds the smallest available can that can fulfill the need (available can  ≥ need). It calculates and adds the difference (waste) between the available can and the actual need. If no can is big enough for a particular need, it skips that need. Finally, the program outputs the total amount of wasted paint.

---
+++ Room painting/Room painting/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+namespace Room_painting
+{
+     class Program
+    {
+        static void Main()
+        {
+            var firstLine = Console.ReadLine().Split();
+            int numberOfcans = int.Parse(firstLine[0]);
+            int numberOfneeds = int.Parse(firstLine[1]);
+
+
+            List<int> canList = new List<int>();
+
+            for (int i = 0; i < numberOfcans; i++) 
+            {
+                //user writes in the numberOfcans one by one until the max
+                int cans = int.Parse(Console.ReadLine());
+                // add to the list everytime the user writes in: so we add cans to the canList
+                canList.Add(cans);
+
+            }
+
+            // we sort the canList so we can find the smallest faster
+            canList.Sort();
+
+            List<int> canNeedsList = new List<int>();
+
+            for (int i =0; i < numberOfneeds; i++)
+            {
+
+                int canNeeds = int.Parse(Console.ReadLine());
+                canNeedsList.Add(canNeeds);
+            }
+
+            // we need to calculate the amount of cans wasted 
+            int TotalWaste = 0;
+
+            foreach (int canNeeds in canNeedsList)
+            { 
+            
+                foreach (int cans in canList)
+                    {
+                    if (cans >= canNeeds)
+                    {
+                        TotalWaste += (cans - canNeeds);
+                        canList.Remove(cans);
+                        break;
+                    }
+                }
+            }
+
+
+            Console.WriteLine(TotalWaste);
+           Console.ReadLine();
+
+        }
+
+    }
+
+}
