c355e8e8607b21b271251fdf910e30b9ae045aa1
Abdi <Dhagaxg@gmail.com>
2025-06-20 00:06:53 +0200

    Baloni

---
+++ Baloni.py
@@ -0,0 +1,15 @@
+# === Initiering ===
+arrows = {}  # Skapa ny map för att döpa unik nummer
+launched = 0  # En räknare
+
+# === Inläsning ===
+N = int(input())  # antal ballonger
+
+# skapa en lsita 
+heights = []
+
+for s in input().split():
+    heights.append(int(s))
+
+
+
